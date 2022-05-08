2f64f7d75dd45bdfc5674ef374fd354287278280
Sathish529 <103407232+Sathish529@users.noreply.github.com>
2022-05-08 19:31:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = var.AWS_REGION
   shared_credentials_file = "~/.aws/credentials"
 }
+terraform {
+ ... some backend config if needed lands here ...
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.74.2" <- you hardcode provider version here
+    }
+  }
+}
